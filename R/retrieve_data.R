545024d953a19c7b9918e6a8a9c47b2e3ed14400
Marcus Codrescu <m.codrescu@outlook.com>
2021-12-18 13:48:47 -0600

    Add Roxygen Comments and Flush Out Package Dependecy Issues

--- R/retrieve_data.R
+++ R/retrieve_data.R
@@ -1,40 +1,39 @@
 
-
-
-# Function parameters: object, api_id, api_key, include_raw
-# object - which knack object should be retrieved
-# api_id/api_key - unique authentication identifiers
-# include_raw - should raw values be included? default is no.
-#   links and image files are only included with raw data
+#' Connect R and Knack
+#'
+#' @param object A string containing the knack object for which to retreive data ex: 'object_23'
+#' @param api_id A string containing the API ID which can be found on the Knack Builder Site under API & Code
+#' @param api_key A string containing the API Key which can be found on the Knack Builder Site under API & Code
+#' @param include_raw A logical value saying whether or not raw field values should be included. Default is no. Note that links and image files are only included in raw fields.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 retrieve_data <-
   function(object,
            api_id,
            api_key,
            include_raw = FALSE) {
-    # Load dependent packages
-    library(jsonlite)
-    library(httr)
-    library(dplyr)
-    library(stringr)
 
     api_base <-
       paste0("https://api.knack.com/v1/objects/", object, "/records")
 
     # First determine the number of pages
-    result <- GET(
+    result <- httr::GET(
       paste0(api_base, "?rows_per_page=1000"),
-      add_headers(
+      httr::add_headers(
         "X-Knack-Application-Id" = api_id,
         "X-Knack-REST-API-Key" = api_key
       )
     )
-    n_pages <- fromJSON(content(result, as = "text"))$total_pages
-    data <- fromJSON(content(result, as = "text"))$records
+    n_pages <- jsonlite::fromJSON(httr::content(result, as = "text"))$total_pages
+    data <- jsonlite::fromJSON(httr::content(result, as = "text"))$records
 
     # Create a function to clean html tags
     dropHTMLTags <- function(column) {
       if (any(grepl(">.+<", column))) {
-        result <- str_remove_all(str_extract(column, ">.+<"), "[><]")
+        result <- stringr::str_remove_all(stringr::str_extract(column, ">.+<"), "[><]")
       } else {
         result <- column
       }
@@ -49,36 +48,36 @@ retrieve_data <-
       }
       # Return the cleaned data
       clean_data <- data %>%
-        select(!contains("raw")) %>%
-        mutate_all(dropHTMLTags)
+        dplyr::select(!dplyr::contains("raw")) %>%
+        dplyr::mutate_all(dropHTMLTags)
       return (clean_data)
     }
 
 
     # If more than 1000 records, loop through the pages to get all the data
     for (i in 2:n_pages) {
-      result <- GET(
+      result <- httr::GET(
         paste0(api_base, "?rows_per_page=1000&page=", i),
-        add_headers(
+        httr::add_headers(
           "X-Knack-Application-Id" = api_id,
           "X-Knack-REST-API-Key" = api_key
         )
       )
       data <- data %>%
-        bind_rows(fromJSON(content(result, as = "text"))$records)
+        dplyr::bind_rows(jsonlite::fromJSON(httr::content(result, as = "text"))$records)
 
     }
 
     # Return raw data if specified
     if (include_raw) {
       return(data %>%
-               mutate_all(dropHTMLTags))
+               dplyr::mutate_all(dropHTMLTags))
     }
 
     # Select only rows that are not "raw" and clean the html tags
     clean_data <- data %>%
-      select(!contains("raw")) %>%
-      mutate_all(dropHTMLTags)
+      dplyr::select(!dplyr::contains("raw")) %>%
+      dplyr::mutate_all(dropHTMLTags)
 
 
     # Return the result
