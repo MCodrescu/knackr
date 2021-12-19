b888cd59e2397fb8e9add5cf21a7d1ae1597a5a6
MCodrescu <m.codrescu@outlook.com>
2021-12-19 08:33:11 -0600

    Fix Issue With API ID and Key Not Accepting Input

--- R/retrieve_filtered_data.R
+++ R/retrieve_filtered_data.R
@@ -28,35 +28,42 @@ retrieve_filtered_data <-
     api_url <-
       paste0("https://api.knack.com/v1/objects/", object, "/records")
 
-    # If dates are given, change to the correct format
-    if (operator %in% c("is after", "if before", "is today")) {
-      try(sapply(value, function(x) {
-        format(as.Date(x), "%m/%d/%Y")
-      }) -> value, silent = TRUE)
-    }
+
 
     # Set filters
-    filters <- list(match = match,
-                    rules = data.frame(
-                      field = filter_field,
-                      operator = operator,
-                      value = value
-                    ))
+    if (!is.null(filter_field)) {
+      # If dates are given, change to the correct format
+      if (operator %in% c("is after", "if before", "is today")) {
+        try(sapply(value, function(x) {
+          format(as.Date(x), "%m/%d/%Y")
+        }) -> value, silent = TRUE)
+      }
 
-    # Convert filters to JSON and URL encode
-    filters_string <-
-      toJSON(filters, auto_unbox = TRUE, pretty = TRUE)
-    api_url <-
-      paste0(api_url,
-             '?rows_per_page=1000&filters=',
-             URLencode(filters_string))
+      filters <- list(
+        match = match,
+        rules = data.frame(
+          field = filter_field,
+          operator = operator,
+          value = value
+        )
+      )
+      # Convert filters to JSON and URL encode
+      filters_string <-
+        toJSON(filters, auto_unbox = TRUE, pretty = TRUE)
+
+      api_url <-
+        paste0(api_url,
+               '?rows_per_page=1000&filters=',
+               URLencode(filters_string))
+    }
+
 
     # Send the GET request
     result <- GET(
       api_url,
       add_headers(
-        "X-Knack-Application-Id" = "60a2a841ac3064001b208e21",
-        "X-Knack-REST-API-Key" = "f7397923-c85f-4391-af98-463f47bb20dd"
+        "X-Knack-Application-Id" = api_id,
+        "X-Knack-REST-API-Key" = api_key
       )
     )
 
@@ -67,15 +74,12 @@ retrieve_filtered_data <-
   }
 
 # Testing
-retrieve_filtered_data("object_2",
-                       api_id = "61be439ed60d72001e68d749",
-                       api_key = "57632271-982d-40ac-acf6-245b7f940dca",
-                       filter_field = "field_6",
-                       match = "and",
-                       operator = "contains",
-                       value = "volvo")
-
-
-
-
-
+retrieve_filtered_data(
+  "object_2",
+  api_id = "61be439ed60d72001e68d749",
+  api_key = "57632271-982d-40ac-acf6-245b7f940dca",
+  filter_field = "field_6",
+  match = "or",
+  operator = "contains",
+  value = "volvo"
+)
