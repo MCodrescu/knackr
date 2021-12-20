7655b9523d782baca8168e037b7202b9038e0316
MCodrescu <m.codrescu@outlook.com>
2021-12-20 13:54:57 -0600

    Combine retrieve_data and retrieve_filtered_data into one function retrieve_records which defaults to retrieving all records.

--- R/retrieve_records.R
+++ R/retrieve_records.R
@@ -1,8 +1,6 @@
-#' Send Filtered Data Requests to Knack
+#' Retrieve Records From Knack
 #'
 #' @param object A string containing the Knack object to pull data from. Ex: 'object_2'
-#' @param api_id A string containing the Knack API ID.
-#' @param api_key A string containing the Knack API key.
 #' @param include_raw A logical stating whether raw fields should be included. Links are only included in raw fields.
 #' @param filter_field A character vector of fields to filter. Can be one or more fields.
 #' @param match A string containing either 'and' or 'or'. This will determine whether the records retrieved match all filters or at least one.
@@ -14,41 +12,38 @@
 #'     Note that dates must be in the format 'YYYY-MM-DD'
 #' @param value A character vector containing the values the operator uses to filter by.
 #'
+#' @importFrom magrittr %>%
 #' @importFrom httr GET
 #' @importFrom httr add_headers
 #' @importFrom httr content
 #' @importFrom jsonlite fromJSON
 #' @importFrom jsonlite toJSON
+#' @importFrom stringr str_remove_all
+#' @importFrom stringr str_extract
 #' @importFrom utils URLencode
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate_all
+#' @importFrom dplyr contains
+#' @importFrom dplyr bind_rows
 #'
 #' @return A data frame of filtered records.
 #' @export
 #'
 #' @examples
-#' retrieve_filtered_data('object_2',
-#' api_id = '61be439ed60d72001e68d749',
-#' api_key = "57632271-982d-40ac-acf6-245b7f940dca",
-#' filter_field = 'field_6',
-#' match = 'or',
-#' operator = c('starts with','ends with'),
-#' value = c('h','i'))
 #'
-#' retrieve_filtered_data("object_2",
-#' api_id = "61be439ed60d72001e68d749",
-#' api_key = "57632271-982d-40ac-acf6-245b7f940dca",
-#' filter_field = "field_13",
-#' match = "and",
-#' operator = c("is after","if before"),
-#' value = c("2021-12-2","2021-12-30"))
-retrieve_filtered_data <-
+retrieve_records <-
   function(object,
-           api_id,
-           api_key,
            include_raw = FALSE,
            filter_field = NA,
            match = NA,
            operator = NA,
            value = NA) {
+    # Check to see if Knack API credentials are set
+    if (is.null(getOption("api_id")) |
+        is.null(getOption("api_key"))) {
+      return (print("Please set API credentials using set_credentials."))
+    }
+
     # Set base url
     api_url <-
       paste0("https://api.knack.com/v1/objects/",
@@ -56,14 +51,25 @@ retrieve_filtered_data <-
              "/records?rows_per_page=1000")
 
 
+    # Create a function to clean html tags
+    dropHTMLTags <- function(column) {
+      if (any(grepl(">.+<", column))) {
+        result <- str_remove_all(str_extract(column, ">.+<"), "[><]")
+      } else {
+        result <- column
+      }
+      result
+    }
+
     # Change dates to correct format
     try(sapply(value, function(x) {
       format(as.Date(x), "%m/%d/%Y")
     }) -> value, silent = TRUE)
 
 
+    # Check to see if any filters are given.
     if (all(sapply(c(filter_field, match, operator, value), is.na))) {
-
+      # Create filters list
       filters <- list(
         match = match,
         rules = data.frame(
@@ -74,26 +80,55 @@ retrieve_filtered_data <-
       )
       # Convert filters to JSON and URL encode
       filters_string <-
-        toJSON(filters, auto_unbox = TRUE, pretty = TRUE)
+        toJSON(filters, auto_unbox = TRUE)
 
+      # Attach filters to the url
       api_url <-
         paste0(api_url,
                '&filters=',
                URLencode(filters_string))
     }
 
-    # Send the GET request
+    # Determine the number of pages
     result <- GET(
       api_url,
       add_headers(
-        "X-Knack-Application-Id" = api_id,
-        "X-Knack-REST-API-Key" = api_key
+        "X-Knack-Application-Id" = getOption("api_id"),
+        "X-Knack-REST-API-Key" = getOption("api_key")
       )
     )
+    n_pages <- fromJSON(content(result, as = "text"))$total_pages
+    data <- fromJSON(content(result, as = "text"))$records
 
-    # Retrieve and return the result
-    data <- fromJSON(content(result, as = "text"))
+
+    # If there is only one page then stop
+    if (n_pages == 1) {
+      # If raw is to be included
+      if (include_raw) {
+        return (data)
+      }
+      # Return the cleaned data
+      clean_data <- data %>%
+        select(!contains("raw")) %>%
+        mutate_all(dropHTMLTags)
+      return (clean_data)
+    }
+
+    # If more than 1000 records, loop through the pages to get all the data
+    for (i in 2:n_pages) {
+      result <- GET(
+        paste0(api_url, "&page=", i),
+        add_headers(
+          "X-Knack-Application-Id" = getOption("api_id"),
+          "X-Knack-REST-API-Key" = getOption("api_key")
+        )
+      )
+      data <- data %>%
+        bind_rows(fromJSON(content(result, as = "text"))$records)
+
+    }
+
+    # Return the result
     data$records
 
   }
-
